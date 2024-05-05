fffd54011b33c3c47d18e1b5c9a742632daa8fc2
osypiukjakub <osypiuk.jakub@gmail.com>
2024-05-05 09:21:22 +0200

    zad3

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Worker worker1 = new Worker("Jacek", 3000, 1, "2020-04-05", "Developer");
         Worker worker2 = new Worker("Bartek", 1600, 2, "2020-02-05", "Developer");
-        Worker worker3 = new Worker("Klara", 2700, 3, "2020-12-20", "Tester");
+        Worker worker3 = new Worker("Klara", 2700, 1, "2020-12-20", "Tester");
         Worker worker4 = new Worker("Sara", 3400, 4, "2019-08-10", "Koordynator");
 
         Manager manager = new Manager("Roman", 10000, 5, "2019-05-10", "CEO");
@@ -26,5 +26,19 @@ public class Main {
                     ", Salary: " + emp.getSalary() + ")");
             emp.work();
         }
+    
+        System.out.println(worker1.getName() + " has code: " + worker1.hashCode());
+        System.out.println(worker2.getName() + " has code: " + worker2.hashCode());
+        System.out.println(worker3.getName() + " has code: " + worker3.hashCode());
+        System.out.println(manager.getName() + " has code: " + manager.hashCode());
+        System.out.println(worker3.getName() + " equals worker1: " + worker3.equals(worker1));
+        System.out.println(worker3.getName() + " equals worker2: " + worker3.equals(worker2));
+        System.out.println(worker3.getName() + " equals manager: " + worker3.equals(manager));
+
+    
+    
+    
+    
     }
+
 }
--- company/abstracts/Employee.java
+++ company/abstracts/Employee.java
@@ -36,4 +36,21 @@ public abstract class Employee implements Employable {
     public int getId() {
         return id;
     }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Employee employee = (Employee) obj;
+        return id == employee.id;
+    }
 }
