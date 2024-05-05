a1c2006b21fce587d391a8dccbd8537fed6dd044
osypiukjakub <osypiuk.jakub@gmail.com>
2024-05-05 09:16:03 +0200

    zad2

--- Main.java
+++ Main.java
@@ -1,28 +1,30 @@
+import java.util.ArrayList;
 import company.models.Manager;
 import company.models.Worker;
+import company.abstracts.Employee;
 
 public class Main {
     public static void main(String[] args) {
-        Worker worker1 = new Worker("Jacek", 3000, 1);
-        Worker worker2 = new Worker("Bartek", 1600, 2);
-        Worker worker3 = new Worker("Klara", 2700, 3);
-        Worker worker4 = new Worker("Sara", 3400, 4);
+        Worker worker1 = new Worker("Jacek", 3000, 1, "2020-04-05", "Developer");
+        Worker worker2 = new Worker("Bartek", 1600, 2, "2020-02-05", "Developer");
+        Worker worker3 = new Worker("Klara", 2700, 3, "2020-12-20", "Tester");
+        Worker worker4 = new Worker("Sara", 3400, 4, "2019-08-10", "Koordynator");
 
-        Manager manager = new Manager("Roman", 10000, 5);
+        Manager manager = new Manager("Roman", 10000, 5, "2019-05-10", "CEO");
 
-        System.out.println("Salary worker_1: " + worker1.getSalary());
-        worker1.work();
+        ArrayList<Employee> employees = new ArrayList<>();
+        employees.add(worker1);
+        employees.add(worker2);
+        employees.add(worker3);
+        employees.add(worker4);
+        employees.add(manager);
 
-        System.out.println("Salary worker_2: " + worker2.getSalary());
-        worker2.work();
-
-        System.out.println("Salary worker_3: " + worker3.getSalary());
-        worker3.work();
-
-        System.out.println("Salary worker_4: " + worker4.getSalary());
-        worker4.work();
-
-        System.out.println("Salary manager: " + manager.getSalary());
-        manager.work();
+        for (Employee emp : employees) {
+            System.out.println("- " + emp.getName() + " (ID: " + emp.getId() +
+                    ", Position: " + emp.getPosition() +
+                    ", Hire date: " + emp.getHireDate() +
+                    ", Salary: " + emp.getSalary() + ")");
+            emp.work();
+        }
     }
 }
