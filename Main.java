8427eed2f5a2d4976adab152410ee9c8fe3c6553
osypiukjakub <osypiuk.jakub@gmail.com>
2024-05-05 09:29:52 +0200

    zad4

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import company.models.Manager;
 import company.models.Worker;
 import company.abstracts.Employee;
@@ -9,15 +12,18 @@ public class Main {
         Worker worker2 = new Worker("Bartek", 1600, 2, "2020-02-05", "Developer");
         Worker worker3 = new Worker("Klara", 2700, 1, "2020-12-20", "Tester");
         Worker worker4 = new Worker("Sara", 3400, 4, "2019-08-10", "Koordynator");
-
+        Worker worker5 = new Worker("Yuval", 2500, 3, "2017-04-16", "Hydraulik");
         Manager manager = new Manager("Roman", 10000, 5, "2019-05-10", "CEO");
+        Manager manager2 = new Manager("Romanek", 120000, 5, "2017-05-10", "CFO");
 
         ArrayList<Employee> employees = new ArrayList<>();
         employees.add(worker1);
         employees.add(worker2);
         employees.add(worker3);
         employees.add(worker4);
+        employees.add(worker5);
         employees.add(manager);
+        employees.add(manager2);
 
         for (Employee emp : employees) {
             System.out.println("- " + emp.getName() + " (ID: " + emp.getId() +
@@ -36,7 +42,42 @@ public class Main {
         System.out.println(worker3.getName() + " equals manager: " + worker3.equals(manager));
 
     
-    
+        double totalSalary = 0;
+        double totalManagerSalary = 0;
+        double totalWorkerSalary = 0;
+
+        Map<Integer, List<Employee>> idMap = new HashMap<>();
+        for (Employee emp : employees) {
+            totalSalary += emp.getSalary();
+            if (emp instanceof Manager) {
+                totalManagerSalary += emp.getSalary();
+            } else if (emp instanceof Worker) {
+                totalWorkerSalary += emp.getSalary();
+            }
+
+            if (idMap.containsKey(emp.getId())) {
+                idMap.get(emp.getId()).add(emp);
+            } else {
+                List<Employee> empList = new ArrayList<>();
+                empList.add(emp);
+                idMap.put(emp.getId(), empList);
+            }
+        }
+
+       
+        System.out.println("Total salary: " + totalSalary);
+        System.out.println("Total manager salary: " + totalManagerSalary);
+        System.out.println("Total worker salary: " + totalWorkerSalary);
+
+        for (Map.Entry<Integer, List<Employee>> entry : idMap.entrySet()) {
+            List<Employee> empList = entry.getValue();
+            if (empList.size() > 1) {
+                System.out.println("Employees with same ID: ");
+                for (Employee emp : empList) {
+                    System.out.println("- " + emp.getName());
+                }
+            }
+        }
     
     
     }
